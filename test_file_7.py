44dce4a7701832fafffe24f86e58526ee9ca7c85
ShreySuri <shreysuri5@gmail.com>
2022-08-06 15:51:16 -0500

    Update test_file_7.py

--- test_file_7.py
+++ test_file_7.py
@@ -4,28 +4,34 @@ elif counter == 4:
     final_message = ""
     while repeat == True:
 
-        input_3 = ""
-        while len(input_3) == 0:
-            input_3 = input("Enter your message one letter at a time. Use '//' to seperate words and '////' as a period. Type 'done' when you are finished with the message.")
+        check = False
+        while check == False:
+            print("")
+            input_3 = input("Enter your message one letter at a time. Use '//', '////', and ' ' as needed. Enter 'done' when finished. ")
             input_3 = input_3.lower()
 
-        if input_3 != "done":
-            
-            check = False
             for i in range (0, 28):
                 if code[i] == input_3:
-                    x = alphabet[i]
-                    final_message = "%s%s" % (final_message, x)
-                    
                     check = True
+                    num = i
                 else:
                     toggle = True
 
             if check == True:
                 toggle = False
+            elif input_3 == "done":
+                check = True
             else:
-                final_message = "%s%s" % (final_message, "[unknown character]")
+                print("")
+                print("Error - '%s' is not an accepted input. Please try again. " % input_3)
+
+        if input_3 != "done":
+            x = alphabet[num]
+            final_message = "%s%s" % (final_message, x)
         else:
             repeat = False
 
     print(final_message)
+        
+
+        
