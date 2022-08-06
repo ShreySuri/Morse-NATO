b63be590b07e34bac82d8cd013549c07419f955d
ShreySuri <shreysuri5@gmail.com>
2022-08-06 15:51:37 -0500

    Update Morse-NATO_1.py

--- Morse-NATO_1.py
+++ Morse-NATO_1.py
@@ -68,41 +68,4 @@ elif counter == 3 or counter == 5:
     print(final_message)
 
 
-elif counter == 4:
 
-    repeat = True
-    final_message = ""
-    while repeat == True:
-
-        input_3 = ""
-        while len(input_3) == 0:
-            input_3 = input("Enter your message one letter at a time. Use '//' to seperate words and '////' as a period. Type 'done' when you are finished with the message.")
-            input_3 = input_3.lower()
-
-        if input_3 != "done":
-            
-            check = False
-            for i in range (0, 28):
-                if code[i] == input_3:
-                    x = alphabet[i]
-                    final_message = "%s%s" % (final_message, x)
-                    
-                    check = True
-                else:
-                    toggle = True
-
-            if check == True:
-                toggle = False
-            else:
-                final_message = "%s%s" % (final_message, "[unknown character]")
-        else:
-            repeat = False
-
-    print(final_message)
-            
-        
-    
-    
-    
-            
-        
