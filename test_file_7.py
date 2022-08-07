7cedec39a513010d6dfe6de76f5f7335b4e6eeda
ShreySuri <shreysuri5@gmail.com>
2022-08-07 10:13:46 -0500

    Update test_file_7.py

--- test_file_7.py
+++ test_file_7.py
@@ -1,37 +1,39 @@
-elif counter == 4:
 
-    repeat = True
+
+counter = 4
+
+if counter == 4:
+
+    check = False
     final_message = ""
-    while repeat == True:
+    while check == False:
+        print("")
+        input_3 = input("Enter your message. Use ' ' '//', and '////' as neccesary. ")
+        input_3 = input_3.lower()
+        input_3 = input_3.split(" ")
+        length = len(input_3)
 
-        check = False
-        while check == False:
-            print("")
-            input_3 = input("Enter your message one letter at a time. Use '//', '////', and ' ' as needed. Enter 'done' when finished. ")
-            input_3 = input_3.lower()
+        for i in range (0, length):
+            word = input_3[i]
+            if word != "//" and word != "////":   
+                word = list(word)
+                len_word = len(word)
+                for j in range (len_word):
+                    for k in range (0, 28):
+                        if word[j] == code[k]:
+                            x = alphabet[k]
+                            final_message = "%s%s" % (final_message, x)
+                            check = True
+                        else:
+                            toggle = True
 
-            for i in range (0, 28):
-                if code[i] == input_3:
-                    check = True
-                    num = i
-                else:
-                    toggle = True
-
-            if check == True:
-                toggle = False
-            elif input_3 == "done":
-                check = True
             else:
-                print("")
-                print("Error - '%s' is not an accepted input. Please try again. " % input_3)
-
-        if input_3 != "done":
-            x = alphabet[num]
-            final_message = "%s%s" % (final_message, x)
-        else:
-            repeat = False
-
-    print(final_message)
-        
-
-        
+                if word == "//":
+                    message_list = "%s%s" % (message_list, " ")
+                    check = True
+                elif word == "////":
+                    message_list = "%s%s" % (message_list, ".")
+                    check = True
+                else:
+                    print("Something went wrong. ")
+    
