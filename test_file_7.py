2129b3902d06664c52a7e5728197ce6976ac5275
ShreySuri <shreysuri5@gmail.com>
2022-08-07 18:52:10 -0500

    Update test_file_7.py

--- test_file_7.py
+++ test_file_7.py
@@ -2,38 +2,16 @@
 
 counter = 4
 
-if counter == 4:
+elif counter == 4:
 
     check = False
     final_message = ""
     while check == False:
         print("")
-        input_3 = input("Enter your message. Use ' ' '//', and '////' as neccesary. ")
+        input_3 = input("Enter your message. Use ' ', '//', and '////' as neccesary. ")
         input_3 = input_3.lower()
         input_3 = input_3.split(" ")
         length = len(input_3)
 
         for i in range (0, length):
-            word = input_3[i]
-            if word != "//" and word != "////":   
-                word = list(word)
-                len_word = len(word)
-                for j in range (len_word):
-                    for k in range (0, 28):
-                        if word[j] == code[k]:
-                            x = alphabet[k]
-                            final_message = "%s%s" % (final_message, x)
-                            check = True
-                        else:
-                            toggle = True
-
-            else:
-                if word == "//":
-                    message_list = "%s%s" % (message_list, " ")
-                    check = True
-                elif word == "////":
-                    message_list = "%s%s" % (message_list, ".")
-                    check = True
-                else:
-                    print("Something went wrong. ")
-    
+            
